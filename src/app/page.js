e256a36139a4902c3086ef02005a3ac2ec2d6098
Ty Streets <54288268+tstreets@users.noreply.github.com>
2024-09-11 10:31:10 -0400

    Add products list with more style and content
    * Add product jsx and class
    
    * Change second product to spam instead of tofu
    
    * Add spinach to products list
    
    * Show products in rows

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import homeStyles from "@/app/page.module.css";
+// No changes made
 
 export default function Home() {
   const headerStyle = {
@@ -69,22 +70,33 @@ export default function Home() {
       <p>Show Null: {String(null)}</p>
       <button onClick={openModal}>View Sun</button>
 
-      <div className={homeStyles.product}>
-        <h3>Tofu</h3>
-        <p>This is definitely tofu.</p>
-        <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/1d1c83da-7e31-4e77-916f-96faf7003173/d2z2tzw-033be9ea-1aee-4a43-bb37-12a615824631.png/v1/fit/w_700,h_700,q_70,strp/a_can_of_spam_by_zellfaze_d2z2tzw-375w-2x.jpg"
-          alt="Real picture of tofu"
-        />
-      </div>
+      <div className={homeStyles.productsList}>
+        <div className={homeStyles.product}>
+          <h3>Tofu</h3>
+          <p>This is definitely tofu.</p>
+          <img
+            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/1d1c83da-7e31-4e77-916f-96faf7003173/d2z2tzw-033be9ea-1aee-4a43-bb37-12a615824631.png/v1/fit/w_700,h_700,q_70,strp/a_can_of_spam_by_zellfaze_d2z2tzw-375w-2x.jpg"
+            alt="Real picture of tofu"
+          />
+        </div>
 
-      <div className={homeStyles.product}>
-        <h3>Tofu</h3>
-        <p>This is definitely tofu.</p>
-        <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/1d1c83da-7e31-4e77-916f-96faf7003173/d2z2tzw-033be9ea-1aee-4a43-bb37-12a615824631.png/v1/fit/w_700,h_700,q_70,strp/a_can_of_spam_by_zellfaze_d2z2tzw-375w-2x.jpg"
-          alt="Real picture of tofu"
-        />
+        <div className={homeStyles.product}>
+          <h3>Spam</h3>
+          <p>This is a can of spam.</p>
+          <img
+            src="https://i1.pickpik.com/photos/829/655/724/spinach-healthy-green-dieting-preview.jpg"
+            alt="A CAN of SPAM"
+          />
+        </div>
+
+        <div className={homeStyles.product}>
+          <h3>Spinach</h3>
+          <p>This is a leaf of spinach.</p>
+          <img
+            src="https://www.ultraboardgames.com/tofu-kingdom/gfx/game7.jpg"
+            alt="Just a measley leaf of spinach"
+          />
+        </div>
       </div>
     </main>
   );
