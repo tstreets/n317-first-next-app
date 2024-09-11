4b92c5cfb8a109d019be783e0ac571d4ea4296e6
Ty Streets <tstreets@iu.edu>
2024-09-11 10:15:58 -0400

    Change second product to spam instead of tofu

--- src/app/page.js
+++ src/app/page.js
@@ -79,11 +79,11 @@ export default function Home() {
       </div>
 
       <div className={homeStyles.product}>
-        <h3>Tofu</h3>
-        <p>This is definitely tofu.</p>
+        <h3>Spam</h3>
+        <p>This is a can of spam.</p>
         <img
-          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/1d1c83da-7e31-4e77-916f-96faf7003173/d2z2tzw-033be9ea-1aee-4a43-bb37-12a615824631.png/v1/fit/w_700,h_700,q_70,strp/a_can_of_spam_by_zellfaze_d2z2tzw-375w-2x.jpg"
-          alt="Real picture of tofu"
+          src="https://i1.pickpik.com/photos/829/655/724/spinach-healthy-green-dieting-preview.jpg"
+          alt="A CAN of SPAM"
         />
       </div>
     </main>
